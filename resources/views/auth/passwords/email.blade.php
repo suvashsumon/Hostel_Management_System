0e3246eec295abf340c0cce7c23865233ad2edfc
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2022-07-18 23:52:56 +0600

    password recovery, reset page and system implemented

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,59 @@
-@extends('layouts.app')
-
+@extends('auth.auth_layout')
+@section('title','Password Recovery')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
+<div class="main-wrapper master2_main" id="app">
+    <div class="page-wrapper full-page">
+        <div class="page-content d-flex align-items-center justify-content-center">
+
+            <div class="row w-100 auth-page mx-0">
+                <div class="col-md-8 col-xl-6 mx-auto">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-4 pr-md-0">
+                                <div class="auth-left-wrapper" style="background-image: url(/images/login_page_img.png)">
+
+                                </div>
+                            </div>
+
+                            <div class="col-md-8 pl-md-0">
+                                <div class="auth-form-wrapper px-4 py-5">
+                                @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
+                                    <a href="#" class="noble-ui-logo d-block mb-2">{{ config('app.name', 'Tinkers Ltd.')
+                                        }}</a>
+                                    <h5 class="text-muted font-weight-normal mb-4">পাসওয়ার্ড রিসেট লিংক পেতে ফর্মটি সাবমিট করুন!!!</h5>
+                                    <form class="forms-sample" method="POST" action="{{ route('password.email') }}">
+                                        @csrf
+                                        <div class="form-group">
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                            <input type="email" name="email" required class="form-control"
+                                                id="exampleInputPhone" placeholder="আপনার ইমেইল দিন">
+                                            @error('email')
+                                            <small class="text-danger">{{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                        <div class="mt-3">
+                                            <button type="submit" class="btn btn-primary mb-md-0 mr-2 mb-2">প্রবেশ
+                                                করুন</button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
+
+
             </div>
+
+
         </div>
+
     </div>
-</div>
-@endsection
+
+    @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,67 @@
-@extends('layouts.app')
-
+@extends('auth.auth_layout')
+@section('title','Password Reset')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+<div class="main-wrapper master2_main" id="app">
+    <div class="page-wrapper full-page">
+        <div class="page-content d-flex align-items-center justify-content-center">
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+            <div class="row w-100 auth-page mx-0">
+                <div class="col-md-8 col-xl-6 mx-auto">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-4 pr-md-0">
+                                <div class="auth-left-wrapper" style="background-image: url(/images/login_page_img.png)">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                </div>
+                            </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                            <div class="col-md-8 pl-md-0">
+                                <div class="auth-form-wrapper px-4 py-5">
+                                    <a href="#" class="noble-ui-logo d-block mb-2">{{ config('app.name', 'Tinkers Ltd.')
+                                        }}</a>
+                                    <h5 class="text-muted font-weight-normal mb-4">নতুন পাসওয়ার্ড সেট করুন!!!</h5>
+                                    <form class="forms-sample" method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="form-group">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                            <input type="number" name="phone_no" required class="form-control"
+                                                id="exampleInputPhone" placeholder="আপনার মোবাইল নাম্বার দিন">
+                                            @error('phone_no')
+                                            <small class="text-danger">{{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" required name="password" class="form-control"
+                                                id="exampleInputPassword1" autocomplete="current-password"
+                                                placeholder="পাসওয়ার্ড দিন">
+                                            @error('password')
+                                            <small class="text-danger">{{ $message }}</small>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" required name="password_confirmation" class="form-control"
+                                                id="exampleInputPassword2" autocomplete="current-password"
+                                                placeholder="পুনরায় পাসওয়র্ড দিন">
+                                        </div>
+                                        <div class="mt-3">
+                                            <button type="submit" class="btn btn-primary mb-md-0 mr-2 mb-2">প্রবেশ
+                                                করুন</button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
+
+
             </div>
+
+
         </div>
+
     </div>
-</div>
-@endsection
+
+    @endsection
